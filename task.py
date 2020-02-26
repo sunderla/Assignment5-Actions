711695c17f9f45e7373e261bb8a1f4091a629432
sunderla <43622185+sunderla@users.noreply.github.com>
2020-02-26 13:17:00 -0800

    added first function

--- task.py
+++ task.py
@@ -1,2 +1,11 @@
+import math
+
+
 def firstrun():
     return "success"
+
+
+def circleArea(radius):
+    pi = math.pi
+    area = pi * (radius * radius)
+    return area
--- tests.py
+++ tests.py
@@ -11,6 +11,14 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+    def test3(self):
+        expected = 50.26548245743669
+        self.assertEqual(expected, task.circleArea(4))
+
+    def test4(self):
+        expected = 4
+        self.assertNotEqual(expected, task.circleArea(4))
+
 
 if __name__ == '__main__':
     unittest.main()
